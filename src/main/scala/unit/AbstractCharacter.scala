3afe36bce0f5f5c2855c0311a3616e55daadaaa2
JTJetc <javier.torres.2001@hotmail.com>
2024-04-06 21:59:30 -0300

    FEAT Added Unit trait and AbstractCharacter abstract class

---
+++ src/main/scala/unit/AbstractCharacter.scala
@@ -0,0 +1,5 @@
+package unit
+
+abstract class AbstractCharacter extends Unit {
+  
+}
---
+++ src/main/scala/unit/Unit.scala
@@ -0,0 +1,8 @@
+package unit
+
+trait Unit {
+  val name: String
+  var life: Int
+  var defense: Int
+  val weight: Float
+}
